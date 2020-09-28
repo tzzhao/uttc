828077e557d25ad2e87e7fcf7d0bfabb6f9948c9
tzhao <thomas.zhao@amadeus.com>
2020-09-28 15:49:25 +0200

    Updates

---
+++ precache-manifest.f812054e79b7c8114751ee60fbdba2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f234a2bb5fb75da4cff93ec78333ac",
+    "url": "/uttc/index.html"
+  },
+  {
+    "revision": "66a67ca11d48edf43c28",
+    "url": "/uttc/static/css/main.23775c2c.chunk.css"
+  },
+  {
+    "revision": "b72ce75b10924ea3aaf6",
+    "url": "/uttc/static/js/2.eb40ad6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uttc/static/js/2.eb40ad6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a67ca11d48edf43c28",
+    "url": "/uttc/static/js/main.54e32f76.chunk.js"
+  },
+  {
+    "revision": "06ea904ec60baf3003c6",
+    "url": "/uttc/static/js/runtime-main.fe4bcce9.js"
+  }
+]);
